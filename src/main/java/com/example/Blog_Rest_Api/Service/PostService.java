42a56e817e66aa977dcd88526b9d46f49d3ae6ec
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2025-11-28 00:12:25 +0530

    Service done

--- src/main/java/com/example/Blog_Rest_Api/Service/PostService.java
+++ src/main/java/com/example/Blog_Rest_Api/Service/PostService.java
@@ -1,8 +1,13 @@
 package com.example.Blog_Rest_Api.Service;
 
+import com.example.Blog_Rest_Api.Entity.Post;
+import com.example.Blog_Rest_Api.Entity.User;
 import com.example.Blog_Rest_Api.Repository.PostRepository;
 import com.example.Blog_Rest_Api.Repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.List;
 
 @Service
 public class PostService {
@@ -15,5 +20,61 @@ public class PostService {
         this.userRepo = userRepo;
     }
 
-    
+    public Post create(Post post){
+        post.setLikedUsers(new HashSet<>());
+        return postRepo.save(post);
+    }
+
+    public List<Post> getAll(){
+        return postRepo.findAll();
+    }
+
+    public Post getById(Integer id){
+        return postRepo.findById(id).get();
+    }
+
+    public Post update(Integer id, Post updated){
+        Post old = postRepo.findById(id).orElse(null);
+        if(old==null) return null;
+
+        old.setTitle(updated.getTitle());
+        old.setContent(updated.getContent());
+
+        return postRepo.save(old);
+    }
+    public boolean delete(Integer id){
+        if(!postRepo.existsById(id)){
+            return false;
+        }
+        postRepo.deleteById(id);
+        return true;
+    }
+
+    //Like Post
+
+    public String likePost(Integer postId,Integer userId){
+        Post post = postRepo.findById(postId).orElse(null);
+        User user = userRepo.findById(userId).orElse(null);
+
+        if(post==null || user ==null) return "Post or user not found";
+
+        post.getLikedUsers().add(user);
+        postRepo.save(post);
+
+        return "Post Liked !";
+    }
+
+    //unlike
+
+    public String UnlikePost(Integer postId,Integer userId){
+        Post post = postRepo.findById(postId).orElse(null);
+        User user = userRepo.findById(userId).orElse(null);
+
+        if(post==null || user == null) return "post or user not found ";
+
+        post.getLikedUsers().remove(user);
+        postRepo.save(post);
+
+        return "Post Unliked !";
+    }
 }
